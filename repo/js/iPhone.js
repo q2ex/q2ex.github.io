e0f691e8c44bf02db97838a886457e5d39b64eb8
q2ex <27537360+q2ex@users.noreply.github.com>
2018-01-26 20:45:30 +0800

    Update iPhone.js

--- repo/js/iPhone.js
+++ repo/js/iPhone.js
@@ -2,7 +2,7 @@ var agent = navigator.userAgent;
 var ios =  agent.match(/.*; CPU (?:iPhone )?OS ([0-9_]*) like Mac OS X[;)]/);
 ios = ios == null ? '6.0' : ios[1].replace(/_/g,'.');
 if (ios.match(/^[78910]($|\.)/) != null) {
-  document.write('<link rel="stylesheet" type="text/css" href="../../repo/css/iPhone.css"');
+  document.write('<link rel="stylesheet" type="text/css" href="../repo/css/iPhone.css"');
 } else {
-  document.write('<link rel="stylesheet" type="text/css" href="../../repo/css/legacy.css"');
+  document.write('<link rel="stylesheet" type="text/css" href="../repo/css/legacy.css"');
 }
