7d4b5d3863da965792087c01e93c4a958bfd656c
q2ex <27537360+q2ex@users.noreply.github.com>
2018-01-26 20:32:36 +0800

    Update developer.js

--- repo/js/developer.js
+++ repo/js/developer.js
@@ -1,6 +1,6 @@
 document.write('\
 	<label>Information</label>\
         <fieldset>\
-            <a href="https://twitter.com/"><img class="icon" src="/icon/twitter-256-black.png"><div><div><label><p>Developer\'s Twitter</p></label></div></div></a>\
+            <a href="https://twitter.com/"><img class="icon" src="../../../icon/twitter-256-black.png"><div><div><label><p>Developer\'s Twitter</p></label></div></div></a>\
          </fieldset>\
 ');
