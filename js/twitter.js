6383824bf6d523e98147530cc9fae3aa462f8f9e
q2ex <27537360+q2ex@users.noreply.github.com>
2018-03-10 00:04:22 +0800

    Delete twitter.js

--- js/twitter.js
+++
@@ -1,6 +0,0 @@
-document.write('\
-	<label>Information</label>\
-        <fieldset>\
-            <a href="https://twitter.com/biibiibbi"><img class="icon" src="../../icon/twitter.png"><div><div><label><p>Developer\'s Twitter</p></label></div></div></a>\
-         </fieldset>\
-');
