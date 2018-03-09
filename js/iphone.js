727ac1620d6482903482e4f2320a3ad3e1725537
q2ex <27537360+q2ex@users.noreply.github.com>
2018-03-10 00:04:29 +0800

    Delete iphone.js

--- js/iphone.js
+++
@@ -1,8 +0,0 @@
-var agent = navigator.userAgent;
-var ios =  agent.match(/.*; CPU (?:iPhone )?OS ([0-9_]*) like Mac OS X[;)]/);
-ios = ios == null ? '6.0' : ios[1].replace(/_/g,'.');
-if (ios.match(/^[78910]($|\.)/) != null) {
-  document.write('<link rel="stylesheet" type="text/css" href="../../css/iphone.css"');
-} else {
-  document.write('<link rel="stylesheet" type="text/css" href="../../css/legacy.css"');
-}
